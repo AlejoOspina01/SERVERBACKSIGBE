b1160f07c1285a21a6817b91dd95ae6ad5414563
AlejoOspina01 <60979547+AlejoOspina01@users.noreply.github.com>
2020-09-27 15:03:19 -0500

    Update listarConfiguracion.php

--- app/componentes/configuracion/listarConfiguracion.php
+++ app/componentes/configuracion/listarConfiguracion.php
@@ -16,21 +16,32 @@ for ($i=0; $i < sizeof($variables); $i++) {
     $variables[$i][1] = trim($variables[$i][1]);
 }
 
-$email = new \SendGrid\Mail\Mail();
-$email->setFrom("alejandro.ospina@correounivalle.edu.co", "Example User");
-$email->setSubject("Sending with Twilio SendGrid is Fun");
-$email->addTo("alejandro.ospina@correounivalle.edu.co", "Example User");
-$email->addContent("text/plain", "and easy to do anywhere, even with PHP");
-$email->addContent(
-    "text/html", "<strong>and easy to do anywhere, even with PHP</strong>"
-);
-$sendgrid = new \SendGrid(getenv('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'));
-try {
-    $response = $sendgrid->send($email);
-    print $response->statusCode() . "\n";
-    print_r($response->headers());
-    print $response->body() . "\n";
-} catch (Exception $e) {
-    echo 'Caught exception: '. $e->getMessage() ."\n";
-}
+
+$request_body = json_decode('{
+  "personalizations": [
+    {
+      "to": [
+        {
+          "email": "alejandro@correounivalle.edu.co"
+        }
+      ],
+      "subject": "Hello, World!"
+    }
+  ],
+  "from": {
+    "email": "alejandro@correounivalle.edu.co"
+  },
+  "content": [
+    {
+      "type": "text/plain",
+      "value": "Hello, World!"
+    }
+  ]
+}');
+$response = $sg->client->mail()->send()->post($request_body);
+echo $response->statusCode();
+echo $response->body();
+echo $response->headers();
+
+
 echo json_encode($variables);
