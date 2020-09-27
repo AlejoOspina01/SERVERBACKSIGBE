2a2666d6a58cc03acc7f14e091f158999a271c9c
AlejoOspina01 <60979547+AlejoOspina01@users.noreply.github.com>
2020-09-27 12:18:14 -0500

    Update listarConfiguracion.php

--- app/componentes/configuracion/listarConfiguracion.php
+++ app/componentes/configuracion/listarConfiguracion.php
@@ -14,10 +14,25 @@ for ($i=0; $i < sizeof($variables); $i++) {
     $variables[$i][1] = trim($variables[$i][1]);
 }
 
-$to = "haloalejo@gmail.com";
-$subject = "Asunto del email";
-$message = "Este es mi primer envío de email con PHP";
- 
-mail($to, $subject, $message);
+$mail = new PHPMailer();
+
+// Settings
+$mail->IsSMTP();
+$mail->CharSet = 'UTF-8';
+
+$mail->Host       = "haloalejo@gmai.com"; // SMTP server example
+$mail->SMTPDebug  = 0;                     // enables SMTP debug information (for testing)
+$mail->SMTPAuth   = true;                  // enable SMTP authentication
+$mail->Port       = 25;                    // set the SMTP port for the GMAIL server
+$mail->Username   = "alejandro.ospina@correounivalle.edu.co"; // SMTP account username example
+$mail->Password   = "Alejoospina123";        // SMTP account password example
+
+// Content
+$mail->isHTML(true);                                  // Set email format to HTML
+$mail->Subject = 'Here is the subject';
+$mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+$mail->send();
 
 echo json_encode($variables);
