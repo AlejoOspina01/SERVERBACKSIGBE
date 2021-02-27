9023a19c6c8f7a5ef4dac7b4b84d759b7af5bef2
AlejoOspina01 <60979547+AlejoOspina01@users.noreply.github.com>
2021-02-27 14:39:11 -0500

    Update buscarReservaTicket.php

--- app/componentes/Tickets/buscarReservaTicket.php
+++ app/componentes/Tickets/buscarReservaTicket.php
@@ -10,31 +10,34 @@ $codigoticket = $_GET['codigoesc'];
 $ticketencontrado = $entityManager->createQuery('SELECT tk FROM Tickets tk WHERE tk.consecutivoticket = ?1')
 	->setParameter(1, $codigoticket)
 	->getSingleResult();
-//
-//$periodoencontrado = $entityManager->createQuery('SELECT pa FROM Periodosacademicos pa WHERE pa.fecha_fin = (SELECT MAX(p.fecha_fin) from Periodosacademicos p)')
-//->getSingleResult();
-//
-//$convocatoriasEncontradas=null;
-//
-//if($tipoticket == 1){
-//	$convocatoriasEncontradas = $entityManager->createQuery('SELECT c FROM Convocatorias c WHERE c.periodosacademicos = ?1 AND c.becas = 1')//
-//	->setParameter(1, $periodoencontrado->getConsecutivo_periodo())
-	//->getSingleResult();
-//}else{
-//	$convocatoriasEncontradas = $entityManager->createQuery('SELECT c FROM Convocatorias c WHERE c.periodosacademicos = ?1 AND c.becas = 2')
-//	->setParameter(1, $periodoencontrado->getConsecutivo_periodo())
-	//->getSingleResult();
-//}
-//
-//$postulacionesEncontradas = $entityManager->createQuery('SELECT p FROM Postulacion p WHERE p.convocatoria = ?1 AND p.usuario = ?2 AND p.estado_postulacion = ?3')
-//->setParameter(1, $convocatoriasEncontradas->getConsecutivoConvocatoria())
-//->setParameter(2, $useriden)
-//->setParameter(3, 'Aprobado')
-//->getSingleResult();
-//
-//$postulacionfound =  array(
-//	'consecutivo_postulacion'     => $postulacionesEncontradas->getConsecutivo_postulacion(),
-//	'fecha'         => $postulacionesEncontradas->getFechapostulacion(),
-//	'estrato'    => $postulacionesEncontradas->getEstrato(),
-//);
-echo json_encode($ticketencontrado->getUsuario()->getIdentifacion());
+
+$periodoencontrado = $entityManager->createQuery('SELECT pa FROM Periodosacademicos pa WHERE pa.fecha_fin = (SELECT MAX(p.fecha_fin) from Periodosacademicos p)')
+->getSingleResult();
+
+$convocatoriasEncontradas=null;
+
+if($ticketencontrado->getTipoTicket() == 'Ticket Refrigerio'){
+	$convocatoriasEncontradas = $entityManager->createQuery('SELECT c FROM Convocatorias c WHERE c.periodosacademicos = ?1 AND c.becas = 1')//
+	->setParameter(1, $periodoencontrado->getConsecutivo_periodo())
+	->getSingleResult();
+}else{
+	$convocatoriasEncontradas = $entityManager->createQuery('SELECT c FROM Convocatorias c WHERE c.periodosacademicos = ?1 AND c.becas = 2')
+	->setParameter(1, $periodoencontrado->getConsecutivo_periodo())
+	->getSingleResult();
+}
+
+$postulacionesEncontradas = $entityManager->createQuery('SELECT p FROM Postulacion p WHERE p.convocatoria = ?1 AND p.usuario = ?2 AND p.estado_postulacion = ?3')
+->setParameter(1, $convocatoriasEncontradas->getConsecutivoConvocatoria())
+->setParameter(2, $ticketencontrado->getUsuario()->getIdentifacion())
+->setParameter(3, 'Aprobado')
+->getSingleResult();
+
+$postulacionfound =  array(
+	'consecutivo_postulacion'     => $postulacionesEncontradas->getConsecutivo_postulacion(),
+	'fecha'         => $postulacionesEncontradas->getFechapostulacion(),
+	'estrato'    => $postulacionesEncontradas->getEstrato(),
+	'nombreest' =>  $ticketencontrado->getUsuario()->getNombre(),
+	'codigoest' => $ticketencontrado->getUsuario()->getCodigoEst(),
+	
+);
+echo json_encode($postulacionfound);
