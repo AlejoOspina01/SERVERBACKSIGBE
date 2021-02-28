d3182ed943a70f6e040c9e2a9f24d914577c0bf9
AlejoOspina01 <60979547+AlejoOspina01@users.noreply.github.com>
2021-02-28 17:05:53 -0500

    Update ticketbyfechauser.php

--- app/componentes/Tickets/ticketbyfechauser.php
+++ app/componentes/Tickets/ticketbyfechauser.php
@@ -4,6 +4,9 @@ header('Access-Control-Allow-Origin: *');
 header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
 // show_roles.php <id>
 require_once "../../../bootstrap.php";
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
+
 
 $idUser = $_GET["idUser"];
 $tipoticketal = $_GET["tipoticket"];
