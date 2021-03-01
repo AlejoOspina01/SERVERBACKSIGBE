9b5645da70a3f59ee6a08617e1af3720d4533e3a
AlejoOspina01 <60979547+AlejoOspina01@users.noreply.github.com>
2021-02-28 21:51:14 -0500

    Update ticketbyfechauser.php

--- app/componentes/Tickets/ticketbyfechauser.php
+++ app/componentes/Tickets/ticketbyfechauser.php
@@ -22,7 +22,7 @@ $ticketencontrado = $entityManager->createQuery('SELECT u FROM Tickets u WHERE u
 ->getSingleResult();
 
 if ($ticketencontrado === null) {
-	echo "No asignacion founbbd.\n";
+	echo "No asignacion founbsdsdbd.\n";
 	exit(1);
 }
 
