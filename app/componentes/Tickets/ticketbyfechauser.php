c1a40959fed5fbbb17aa8c8ed4a4364a33c47977
AlejoOspina01 <60979547+AlejoOspina01@users.noreply.github.com>
2021-02-28 21:37:06 -0500

    Update ticketbyfechauser.php

--- app/componentes/Tickets/ticketbyfechauser.php
+++ app/componentes/Tickets/ticketbyfechauser.php
@@ -22,7 +22,7 @@ $ticketencontrado = $entityManager->createQuery('SELECT u FROM Tickets u WHERE u
 ->getSingleResult();
 
 if ($ticketencontrado === null) {
-	echo "No asignacion foundssd.\n";
+	echo "No asignacion found.\n";
 	exit(1);
 }
 
