b1c20632574956fa28eac1961ef97df10212ca19
AlejoOspina01 <60979547+AlejoOspina01@users.noreply.github.com>
2021-02-28 20:43:54 -0500

    Update ticketbyfechauser.php

--- app/componentes/Tickets/ticketbyfechauser.php
+++ app/componentes/Tickets/ticketbyfechauser.php
@@ -14,7 +14,6 @@ $fechahoy = new \DateTime('now');
 $fechafinal= $fechahoy->format('Y-m-d');
 $fechainicialhoy = '2021-02-28 00:00:00';
 $fechafinalhoy = '2021-02-28 23:59:59';
-var_dump($fechahoy);
 
 // $ticketencontrado = $entityManager->createQuery('SELECT u FROM Tickets u WHERE u.fechacompra BETWEEN ?2 AND ?3 AND u.tipoTicket = ?4 AND u.usuario = ?1')
 $ticketencontrado = $entityManager->createQuery('SELECT u FROM Tickets u WHERE u.fechacompra BETWEEN ?2 AND ?3')
